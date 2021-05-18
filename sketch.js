d2b7d604be51a5e8720c06b45ef13355d77d7954
moupramanik <77617522+moupramanik@users.noreply.github.com>
2021-05-18 16:29:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,43 +11,45 @@ function setup(){
     canvas = createCanvas(800,600);
 
     //create 4 different surfaces
-surface1=createSprite();
-surface1.shapecolor="red";
-surface2=createSprite();
-surface2.shapecolor="green";
-surface3=createSprite();
-surface3.shapecolor="blue";
-surface4=createSprite();
-surface4.shapecolor="yellow";
+surface1=createSprite(0,580,360,30);
+surface1.shapeColor="red";
+surface2=createSprite(295,580,200,30);
+surface2.shapeColor="green";
+surface3=createSprite(515,580,200,30);
+surface3.shapeColor="blue";
+surface4=createSprite(740,580,200,30);
+surface4.shapeColor="yellow";
 
     //create box sprite and give velocity
-movingbox=createSprite();
-movingbox.shapecolor="white";
+movingbox=createSprite(random(20,750),100,40,40);
+movingbox.shapeColor="white";
 movingbox.velocityX=3;
-createSprite(random(20,750));
+movingbox.velocityY=8;
+
 }
 
 function draw() {
     background(rgb(169,169,169));
- movingbox.x=World.mouseX;
- movingbox.y=World.mouseY;   
-    //create edgeSprite
-movingbox.bounceoff(edges);
-movingbox.bounceoff(surface1);
-movingbox.bounceoff(surface2);
-movingbox.bounceoff(surface3);
-movingbox.bounceoff(surface4);
-
+    edges=createEdgeSprites();
+    movingbox.bounceOff(edges);
+ 
     //add condition to check if box touching surface and make it box
-if(movingbox.isTouching(surface1)||movingbox.isTouching(surface2)||movingbox.isTouching(surface3)||movingbox.isTouching(surface4)){
-    movingbox.shapecolor="change";
-    movingbox.velocityX=0;
-    music.stop();
+if(movingbox.isTouching(surface1) && movingbox.bounceOff(surface1)){
+    movingbox.shapeColor="red";
+    music.play();
 }
 
-
-
-
-
+if(movingbox.isTouching(surface2) && movingbox.bounceOff(surface2)){
+    movingbox.shapeColor="green";
+    music.play();
+}
+if(movingbox.isTouching(surface3) && movingbox.bounceOff(surface3)){
+    movingbox.shapeColor="blue";
+    music.play();
+}
+    if(movingbox.isTouching(surface4) && movingbox.bounceOff(surface4)){
+        movingbox.shapeColor="yellow";
+        music.play();
+    }       
 drawSprites();
 }
